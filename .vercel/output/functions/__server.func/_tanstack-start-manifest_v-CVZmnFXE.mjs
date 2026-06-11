0fd97fb792fccf11bc55fe903fb4480883f09514
Ayush Chauhan <ayushch435@gmail.com>
2026-06-11 18:21:35 +0530

    vercel

---
+++ .vercel/output/functions/__server.func/_tanstack-start-manifest_v-CVZmnFXE.mjs
@@ -0,0 +1,20 @@
+//#region node_modules/.nitro/vite/services/ssr/assets/_tanstack-start-manifest_v-CVZmnFXE.js
+var tsrStartManifest = () => ({ routes: {
+	__root__: {
+		filePath: "/Users/ayushchauhan/codes/untitled folder/Alpha Wellness Gateway/src/routes/__root.tsx",
+		children: ["/"],
+		preloads: ["/assets/index-iUQCRmA3.js"],
+		scripts: [{ attrs: {
+			type: "module",
+			async: !0,
+			src: "/assets/index-iUQCRmA3.js"
+		} }]
+	},
+	"/": {
+		filePath: "/Users/ayushchauhan/codes/untitled folder/Alpha Wellness Gateway/src/routes/index.tsx",
+		children: void 0,
+		preloads: ["/assets/routes-CT_G7enL.js"]
+	}
+} });
+//#endregion
+export { tsrStartManifest };
